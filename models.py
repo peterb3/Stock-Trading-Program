f402e596521b32f427d0c2b01445b882dc6cae06
peterb3 <pbattisti20@gmail.com>
2024-11-08 16:01:08 -0800

    commenting code for py files for app

--- models.py
+++ models.py
@@ -2,12 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Defining Stock database entry; contains a symbol, quantity, and purchase price
 class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     symbol = db.Column(db.String(10), nullable=False)
     quantity = db.Column(db.Float, nullable=False)
     purchase_price = db.Column(db.Float, nullable=False)
 
+# Defining user money entry; contains the free cash amount
 class Balance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
--- stock_service.py
+++ stock_service.py
@@ -2,11 +2,14 @@ import requests
 from dotenv import load_dotenv
 import os
 
+# import stock API key from env file
 load_dotenv()
 
 class StockService:
+    # set API key in class
     API_KEY = os.environ.get('STOCK_API_KEY')
 
+    # call stockdata.org API endpoint for stock symbol, pull data from JSON response, and return price as float.
     @staticmethod
     def get_stock_price(symbol):
         url = f"https://api.stockdata.org/v1/data/quote?symbols={symbol}&api_token={StockService.API_KEY}"
